c530b4868bcf5a7c515f08b690d0dc205c98dc6f
Ezzat Hegazy <ezzat22hegazy@gmail.com>
2023-11-06 17:47:16 +0200

    Add files via upload

---
+++ models/ordinal_dummies.py
@@ -0,0 +1,18 @@
+land_surface_condition_dummies = {'Flat'          : 0, 
+                                  'Moderate slope': 1, 
+                                  'Steep slope'   : 2}
+
+roof_type_dummies = {'Bamboo/Timber-Heavy roof': 0,
+                     'Bamboo/Timber-Light roof': 1,
+                     'RCC/RB/RBC'              : 2 }
+
+ground_floor_type_dummies = {'Brick/Stone': 0, 
+                             'Mud'         : 1, 
+                             'RC'          : 2, 
+                             'Timber'      : 3}
+
+position_dummies = {'Attached-1 side': 0,
+                    'Attached-2 side': 1,
+                    'Attached-3 side': 2,
+                    'Not attached'   : 3}
+
